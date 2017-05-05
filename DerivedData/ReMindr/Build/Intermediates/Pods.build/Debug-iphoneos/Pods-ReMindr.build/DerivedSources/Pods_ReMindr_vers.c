05688598ee87c08f15dadddcb935167b8e4619ae
kobe24u <kobe24u@vip.qq.com>
2017-05-05 17:59:55 +1000

    committed before pulling

---
+++ DerivedData/ReMindr/Build/Intermediates/Pods.build/Debug-iphoneos/Pods-ReMindr.build/DerivedSources/Pods_ReMindr_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_ReMindrVersionString[];
+ extern const double Pods_ReMindrVersionNumber;
+
+ const unsigned char Pods_ReMindrVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_ReMindr  PROJECT:Pods-1" "\n";
+ const double Pods_ReMindrVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ DerivedData/ReMindr/Build/Intermediates/ReMindr.build/Debug-iphoneos/ReMindrUITests.build/Script-089A503AA01E73A5F48F4914.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+"${SRCROOT}/Pods/Target Support Files/Pods-ReMindrUITests/Pods-ReMindrUITests-frameworks.sh"
+
