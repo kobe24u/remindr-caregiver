3c8f843bba11056f43a7b096d5dd5590e238b278
kobe24u <kobe24u@vip.qq.com>
2017-05-07 12:59:55 +1000

    committed before pushing

--- ReMindr/MainMenuViewController.swift
+++ ReMindr/MainMenuViewController.swift
@@ -43,15 +43,30 @@ class MainMenuViewController: UIViewController {
         let context = LAContext()
         var error: NSError?
         
-        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
-            let reason = "Identify yourself!"
-            
-            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
-                [unowned self] success, authenticationError in
+//        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+//            let reason = "Identify yourself!"
+//            
+//            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+//                [unowned self] success, authenticationError in
+//                
+//                if success{
+//                    DispatchQueue.main.async {
+//                       self.performSegue(withIdentifier: "ShowGeofencingMapSegue", sender: self)
+//                        // let destinationVC = segue.destination as! GeofencingViewController
+//                        //destinationVC.fromSegue = true
+//                    }
+//                }
+//                else {
+//                    let ac = UIAlertController(title: "Authentication failed", message: "Sorry!", preferredStyle: .alert)
+//                    ac.addAction(UIAlertAction(title: "OK", style: .default))
+//                    self.present(ac, animated: true)
+//                }                }
+//        } else {
+            context.evaluatePolicy(LAPolicy.deviceOwnerAuthentication, localizedReason: "Please authenticate to proceed.") { [weak self] (success, error) in
                 
                 if success{
                     DispatchQueue.main.async {
-                       self.performSegue(withIdentifier: "ShowGeofencingMapSegue", sender: self)
+                        self?.performSegue(withIdentifier: "ShowGeofencingMapSegue", sender: self)
                         // let destinationVC = segue.destination as! GeofencingViewController
                         //destinationVC.fromSegue = true
                     }
@@ -59,36 +74,34 @@ class MainMenuViewController: UIViewController {
                 else {
                     let ac = UIAlertController(title: "Authentication failed", message: "Sorry!", preferredStyle: .alert)
                     ac.addAction(UIAlertAction(title: "OK", style: .default))
-                    self.present(ac, animated: true)
-                }                }
-        } else {
-            context.evaluatePolicy(LAPolicy.deviceOwnerAuthentication, localizedReason: "Please authenticate to proceed.") { [weak self] (success, error) in
-                
-                guard success else {
-                    DispatchQueue.main.async() {
-                        // show something here to block the user from continuing
-                        let ac = UIAlertController(title: "Authentication failed", message: "Sorry!", preferredStyle: .alert)
-                        ac.addAction(UIAlertAction(title: "OK", style: .default))
-                        self?.present(ac, animated: true)
-                    }
-                    
-                    return
+                    self?.present(ac, animated: true)
                 }
-                
-                DispatchQueue.main.async() {
-                    // do something here to continue loading your app, e.g. call a delegate method
-                    self?.performSegue(withIdentifier: "ShowGeofencingMapSegue", sender: self)
-                    
-                    //let destinationVC = segue.destination as! GeofencingViewController
-//                    destinationVC.fromSegue = true
-                }
+//                
+//                guard success else {
+//                    DispatchQueue.main.async() {
+//                        // show something here to block the user from continuing
+//                        let ac = UIAlertController(title: "Authentication failed", message: "Sorry!", preferredStyle: .alert)
+//                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+//                        self?.present(ac, animated: true)
+//                    }
+//                    
+//                    return
+//                }
+//                
+//                DispatchQueue.main.async() {
+//                    // do something here to continue loading your app, e.g. call a delegate method
+//                    self?.performSegue(withIdentifier: "ShowGeofencingMapSegue", sender: self)
+//                    
+//                    //let destinationVC = segue.destination as! GeofencingViewController
+////                    destinationVC.fromSegue = true
+//                }
             }
             
             
             //                let ac = UIAlertController(title: "Touch ID not available", message: "Your device is not configured for Touch ID.", preferredStyle: .alert)
             //                ac.addAction(UIAlertAction(title: "OK", style: .default))
             //                present(ac, animated: true)
-        }
+        
     }
     
 
