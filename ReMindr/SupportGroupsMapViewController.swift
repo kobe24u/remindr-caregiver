7a693b3e8c9d1530b34ab4ca14e56ec1e036f31b
Priyanka Gopakumar <pgop6@student.monash.edu>
2017-05-07 12:49:07 +1000

    committed before pulling

--- ReMindr/SupportGroupsMapViewController.swift
+++ ReMindr/SupportGroupsMapViewController.swift
@@ -13,7 +13,7 @@ class SupportGroupsMapViewController: UIViewController, MKMapViewDelegate, CLLoc
 
     @IBOutlet weak var mapView: MKMapView!
     
-    var BASE_URL: String = "http://35.161.212.185"
+    var BASE_URL: String = "http://35.164.11.87"
     var SUPPORT_METHOD: String = "/communitysupportinfo/"
     
     let locationManager = CLLocationManager()
@@ -21,6 +21,9 @@ class SupportGroupsMapViewController: UIViewController, MKMapViewDelegate, CLLoc
     var selectedSupportGroup: SupportGroup?
     var myLatitude: Double
     var myLongitude: Double
+    
+    // creating a view to display a progress spinner while data is being loaded from the server
+    var progressView = UIView()
     
     required init?(coder aDecoder: NSCoder) {
         myLatitude = -37.8
@@ -44,6 +47,7 @@ class SupportGroupsMapViewController: UIViewController, MKMapViewDelegate, CLLoc
         if Reachability.isConnectedToNetwork() == true      // if data network exists
         {
             print("Internet connection OK")
+            setProgressView() // setup progress view
             readAllSupportGroupInfo()
 //            readPatientCoordinatesOnce()
 //            plotPatientLocationOnMap()
@@ -78,6 +82,7 @@ class SupportGroupsMapViewController: UIViewController, MKMapViewDelegate, CLLoc
     
     func readAllSupportGroupInfo()
     {
+        self.view.addSubview(progressView)
         var requestURL: String?
         requestURL = BASE_URL + SUPPORT_METHOD + "\(myLatitude)/\(myLongitude)"
         print ("request url is \(requestURL)")
@@ -110,6 +115,10 @@ class SupportGroupsMapViewController: UIViewController, MKMapViewDelegate, CLLoc
             let result = try JSONSerialization.jsonObject(with: groupsJSON as Data, options: JSONSerialization.ReadingOptions.mutableContainers)
             if let results = result as? NSArray
             {
+                DispatchQueue.main.async(){
+                    self.stopProgressView()
+                }
+                
                 for groupResult in results
                 {
                     if let currentResult: NSDictionary = groupResult as! NSDictionary
@@ -251,6 +260,64 @@ class SupportGroupsMapViewController: UIViewController, MKMapViewDelegate, CLLoc
         alertController.addAction(alertAction)
         self.present(alertController, animated: true, completion: nil)
     }
+    
+    /*
+     Setting up the progress view that displays a spinner while the serer data is being downloaded.
+     The view uses an activity indicator (a spinner) and a simple text to convey the information.
+     Source: YouTube
+     Tutorial: Swift - How to Create Loading Bar (Spinners)
+     Author: Melih Şimşek
+     URL: https://www.youtube.com/watch?v=iPTuhyU5HkI
+     */
+    func setProgressView()
+    {
+        // setting the UI specifications
+        var grayColor = UIColor(colorLiteralRed: 239/255, green: 239/255, blue: 244/255, alpha: 1)
+        var blueColor = UIColor(colorLiteralRed: 45/255, green: 86/255, blue: 105/255, alpha: 1)
+        
+        
+        self.progressView = UIView(frame: CGRect(x: 0, y: 0, width: 250, height: 50))
+        self.progressView.backgroundColor = grayColor
+        //self.progressView.backgroundColor = UIColor.lightGray
+        self.progressView.layer.cornerRadius = 10
+        let wait = UIActivityIndicatorView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        wait.color = blueColor
+        //wait.color = UIColor(red: 254/255, green: 218/255, blue: 2/255, alpha: 1)
+        wait.hidesWhenStopped = false
+        wait.startAnimating()
+        
+        let message = UILabel(frame: CGRect(x: 60, y: 0, width: 200, height: 50))
+        message.text = "Retrieving data..."
+        message.textColor = blueColor
+        //message.textColor = UIColor(red: 254/255, green: 218/255, blue: 2/255, alpha: 1)
+        
+        self.progressView.addSubview(wait)
+        self.progressView.addSubview(message)
+        self.progressView.center = self.view.center
+        self.progressView.tag = 1000
+        
+    }
+    
+    /*
+     This method is invoked to remove the progress spinner from the view.
+     Source: YouTube
+     Tutorial: Swift - How to Create Loading Bar (Spinners)
+     Author: Melih Şimşek
+     URL: https://www.youtube.com/watch?v=iPTuhyU5HkI
+     */
+    func stopProgressView()
+    {
+        let subviews = self.view.subviews
+        self.progressView.removeFromSuperview()
+//        for subview in subviews
+//        {
+//            if subview.tag == 1000
+//            {
+//                subview.removeFromSuperview()
+//            }
+//        }
+    }
+
     
     /*
     // MARK: - Navigation
